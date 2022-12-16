95c72ce35a92ed31b5ec6e816bb7681bbf958801
Shaka98 <eiveredilson1998@gmail.com>
2022-12-16 12:26:42 -0500

    Cherish ify

--- cherish_surya.mk
+++ cherish_surya.mk
@@ -21,10 +21,10 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from surya device
 $(call inherit-product, device/xiaomi/surya/device.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common Cherish stuff.
+$(call inherit-product, vendor/cherish/config/common_full_phone.mk)
 
-PRODUCT_NAME := lineage_surya
+PRODUCT_NAME := cherish_surya
 PRODUCT_DEVICE := surya
 PRODUCT_BRAND := POCO
 PRODUCT_MODEL := POCO X3 NFC
@@ -34,3 +34,14 @@ PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
 # Blur
 TARGET_ENABLE_BLUR := true
+
+# Boot animation
+TARGET_BOOT_ANIMATION_RES := 1080
+
+# GApps
+$(call inherit-product-if-exists, vendor/gapps/core/config.mk)
+
+# Maintainer
+CHERISH_BUILD_TYPE := UNOFFICIAL
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
+    ro.cherish.maintainer=Shaka98
