4b11b3006557e03ae1123a58bdb01f4e9d234f26
Shaka98 <eiveredilson1998@gmail.com>
2023-01-24 16:21:50 -0500

    Project Elixir Bringup

--- aosp_surya.mk
+++ aosp_surya.mk
@@ -11,10 +11,10 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from surya device
 $(call inherit-product, device/xiaomi/surya/device.mk)
 
-# Inherit some common Palyrim stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common Project-Elixir stuff.
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 
-PRODUCT_NAME := lineage_surya
+PRODUCT_NAME := aosp_surya
 PRODUCT_DEVICE := surya
 PRODUCT_BRAND := POCO
 PRODUCT_MODEL := POCO X3 NFC
@@ -28,10 +28,14 @@ TARGET_ENABLE_BLUR := true
 # Boot animation
 TARGET_BOOT_ANIMATION_RES := 1080
 
-#Maintainer
-WITH_GMS := true
-PALYRIM_BUILD_TYPE := UNOFFICIAL
-PALYRIM_MAINTAINER := Shaka98
+# Project-Elixir Official Stuff
+IS_PHONE := true
+TARGET_SUPPORTS_QUICK_TAP := true
+CUSTOM_BUILD_TYPE := UNOFFICIAL
+TARGET_SUPPORTS_GOOGLE_RECORDER := false
+TARGET_INCLUDE_STOCK_ACORE := false
+TARGET_INCLUDE_LIVE_WALLPAPERS := false
+TARGET_FACE_UNLOCK_SUPPORTED := true
 
 PRODUCT_SYSTEM_PROPERTIES += \
     ro.product.mod_device=surya_global
